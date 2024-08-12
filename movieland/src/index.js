9ace3ccf62fe899124f508958c7ad024083e3f35
Swayam Prakash Sahu <91021799+SwayamSahu@users.noreply.github.com>
2024-08-12 11:43:38 +0530

    pushing project

---
+++ movieland/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+//ReactDOM.render(<App/>, document.getElementById('root'));
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App/>);
